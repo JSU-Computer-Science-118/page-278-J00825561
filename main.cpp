9507e20d724eb0427af5d123a550ec2a24c9143c
Xavier <117471614+J00825561@users.noreply.github.com>
2022-11-19 20:25:17 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
-// write your code here
+Input: Amari	4
+Katrina	11
+Lexus	19
+-1
+3.50
+Output: Total number of boxes sold: 34
+Total money made: $119.00
+Average number of boxes sold by each person: 11
+  ````````````````````````````````````````````````````````````````````
+  // Xavier Thompson J00825561 November 19th, 2022 
+  #include <iostream>
+  using namespace std;
+  int main ()
+{
+    int student Amari, Katrina & Lexus;
+    tripleboxesSold;
+    triple Calculate;
+    triple sum1 = 3.50;
+    cout << "Enter the number of students(1-3);
+    cin >> Studentname; 
+    cout << "Enter boxes sold:";
+    cin >> Boxessold; while (Studentname!=3)
+    {
+      case 1: sum1 += (3.50 * 4 * Calculate);
+      cout <<"The sale for this student 1, Amari is $14." << sum1 << endl;
+      
+      case 2: sum2 += (3.50 * 11 *Calculate);
+      cout <<" The sale for student 2 name Katrina is $38.50." << sum2 << endl; 
+      
+      case 3: sum3 += (3.50 * 19 - 1 * Calculate);
+      cout <<"The sale foar student 3 name Lexus is $66.5 total or $63." << sum3 << endl;
+      cout << "Enter boxes sold:";
+      cin >> "34 boxes sold";
+      cout <<"Student 1 $14. " sum1 << endl;
+      cout <<"Student 2 $38.50. " sum2 << endl;
+      cout <<"Student 3 $63. " sum3 << endl; 
+    }
+    }
+      return 0;
+}
+    
